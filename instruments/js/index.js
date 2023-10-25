0f2b2c314e8493262f83cf0dabeb83a36066446e
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-10-25 17:29:57 +0100

    final commit

--- instruments/js/index.js
+++ instruments/js/index.js
@@ -1,10 +1,47 @@
-var numberOfInstruments = document.querySelectorAll(".instrument").length;
+document.addEventListener('DOMContentLoaded', function () {
 
-for(i=0; i<numberOfInstruments; i++) {
-    document.querySelectorAll(".instrument")[i].addEventListener("click", function() {
-        alert("You clicked me!")
+const guitarSound = new Audio('sounds/guitar.wav');
+const drumSound = new Audio('sounds/drum.wav');
+const bassSound = new Audio('sounds/bass.wav');
+const fluteSound = new Audio('sounds/flute.wav');
+const pianoSound = new Audio('sounds/piano.wav');
+const saxSound = new Audio('sounds/sax.wav');
+
+const playSoundButton = document.getElementById('playSoundButton');
+
+playSoundButton.addEventListener('click', function (){
+    const soundToPlay =  playSoundButton.dataset.sound;
+        
+        switch (soundToPlay) {
+            case "guitarSound":
+                guitarSound.play;
+                break;
+            
+            case "drum":
+                drumSound.play();
+                break;
+            
+            case "bass":
+                bassSound.play();
+                break;
+
+            case "flute":
+                fluteSound.play();
+                break;
+
+            case "piano":
+                pianoSound.play();
+                break;
+
+            case "sax":
+                saxSound.play();
+                break;
+        
+            default:
+                console.log("error")
+                break;
+        }
+
     });
-}
 
-
-
+});
